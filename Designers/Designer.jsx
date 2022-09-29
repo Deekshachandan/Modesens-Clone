4d5a39660652f7aa4e77c7cc93b37c6ae3ae2f6f
Deeksha purohit <105900022+Deekshachandan@users.noreply.github.com>
2022-09-29 18:12:44 +0530

    Add files via upload
    Added Home/ kids/mens/women/Designer/All Routes Pages

---
+++ Designers/Designer.jsx
@@ -0,0 +1,13 @@
+
+import DesignerSlider from "./Slider"
+import DesignerImages from "./DesignerSub/Designersub"
+
+export default function Designers(){
+    return(
+        <div>
+        <h2>Designers Page</h2>
+        <DesignerSlider />
+        <DesignerImages />
+        </div>
+    )
+}
---
+++ Home/Home.jsx
@@ -0,0 +1,13 @@
+
+
+import HomeSlider from "./HomeSlider";
+import HomeSmallImages from "./StartImages/Images";
+
+export default function Home(){
+  return(      
+  <div>
+<HomeSlider />
+<HomeSmallImages />
+</div>
+)
+}
